1a63fa350cd77fb4e6ed5128f0dbe6d0a89e6938
Mike <truenorthartllc@gmail.com>
2026-06-26 19:52:49 -0600

    fix: point presentations iframe to correct static file

--- frontend/src/pages/ListingPresentationPage.jsx
+++ frontend/src/pages/ListingPresentationPage.jsx
@@ -17,7 +17,7 @@ export default function ListingPresentationPage() {
       </header>
 
       <iframe
-        src="/presentations/index.html"
+        src="/listing-presentation.html"
         style={{ position: 'fixed', top: 56, left: 0, right: 0, bottom: 0, width: '100%', height: 'calc(100vh - 56px)', border: 'none' }}
         title="Listing Presentation"
       />
